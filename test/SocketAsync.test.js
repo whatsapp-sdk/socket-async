1a1d9557c38cb40597659a3250a00afa2c384bce
whatsappsdk <test@qq.com>
2024-03-17 08:57:57 +0800

    test: add test readAndClearAsync

--- test/SocketAsync.test.js
+++ test/SocketAsync.test.js
@@ -1,6 +1,28 @@
 const assert = require('assert');
 const SocketAsync = require('../SocketAsync');
 describe('SocketAsync', function () {
+  it('readAndClearAsync get google.com', async () => {
+    const socket = new SocketAsync();
+
+    let host, port;
+
+    host = 'google.com';
+    port = 80;
+    await socket.connectAsync({
+      host,
+      port,
+      timeout: 1000 * 5,
+      proxy: {
+        host: '127.0.0.1',
+        port: 1086,
+      },
+    });
+    socket.write('GET / HTTP/1.1\r\nHost: google.com\r\n\r\n');
+    const chunks = await socket.readAndClearAsync((data) => data.length);
+    const str = chunks.toString();
+    assert(str.includes('HTTP/1.1 301 Moved Permanently'));
+  });
+
   it('getaddrinfo ENOTFOUND localhost', async () => {
     const socket = new SocketAsync();
 
@@ -11,7 +33,7 @@ describe('SocketAsync', function () {
       await socket.connectAsync({
         host,
         port,
-        timeout: 1000 * 5,
+        timeout: 1000 * 2,
       });
     } catch (e) {
       assert(!!e.message.match('ENOTFOUND'));
@@ -28,30 +50,11 @@ describe('SocketAsync', function () {
       await socket.connectAsync({
         host,
         port,
-        timeout: 1000 * 5,
+        timeout: 1000 * 2,
       });
     } catch (e) {
       console.error(e);
       assert(!!e.message.match('ECONNECTTIMEOUT'));
     }
-  });
-
-  it('ECONNECTTIMEOUT google.com', async () => {
-    const socket = new SocketAsync();
-
-    let host, port;
-
-    host = 'google.com';
-    port = 443;
-    await socket.connectAsync({
-      host,
-      port,
-      timeout: 1000 * 5,
-      proxy: {
-        host: '127.0.0.1',
-        port: 1086,
-      },
-    });
-    console.log('connect google.com with proxy successed.');
   });
 });
